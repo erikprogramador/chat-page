08fd16f835dc8a8b2041f361f861df914cdb0f0b
Erik Vanderlei Fernandes <erik.vanderlei.programador@outlook.com>
2017-04-25 16:18:51 -0300

    Styling add message component

--- src/components/AddMessage.vue
+++ src/components/AddMessage.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>Add Message</div>
+  <form class="send-form">
+    <input type="text" class="send-input" placeholder="Send a message to UserChat">
+    <button class="btn-send">
+      <img src="../assets/send.svg" alt="Send">
+    </button>
+  </form>
 </template>
 
 <script>
